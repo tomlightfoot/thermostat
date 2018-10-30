1da1edfb92c2c197549dd0e480c0113e7a386fc1
tomlightfoot <tomlightfoot96@gmail.com>
2018-10-30 15:06:40 +0000

    energy usage added

--- src/thermostat.js
+++ src/thermostat.js
@@ -1,9 +1,11 @@
 function Thermostat() {
-  this.temperature = 20;
+  this.DEFAULT_TEMPERATURE = 20;
+  this.temperature = this.DEFAULT_TEMPERATURE;
   this.MINIMUM_TEMPERATURE = 10;
   this.PowerSavingMode = true;
   this.MAX_LIMIT_PSM_ON = 25;
   this.MAX_LIMIT_PSM_OFF = 32;
+  this.MEDIUM_ENERGY_USAGE_LIMIT = 18;
 };
 
 Thermostat.prototype.getCurrentTemperature = function () {
@@ -46,3 +48,17 @@ Thermostat.prototype.switchPowerSavingModeOff = function () {
 Thermostat.prototype.switchPowerSavingModeOn = function () {
   this.PowerSavingMode = true;
 };
+
+Thermostat.prototype.resetTemperature = function() {
+  this.temperature = this.DEFAULT_TEMPERATURE;
+};
+
+Thermostat.prototype.energyUsage = function() {
+  if (this.temperature < this.MEDIUM_ENERGY_USAGE_LIMIT) {
+    return 'low-usage';
+  }
+  if (this.temperature >= this.MEDIUM_ENERGY_USAGE_LIMIT && this.temperature <= this.MAX_LIMIT_PSM_ON) {
+    return 'medium-usage';
+  }
+  return 'high-usage';
+}
